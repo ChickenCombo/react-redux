b21aea3095c9a71881a366df0b5dab388228fe11
Lester Cuadra <lester.cuadra24@gmail.com>
2023-01-31 15:31:04 +0800

    feat: added item container

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,17 +1,25 @@
 import './App.css'
-import CakeContainer from './components/CakeContainer'
+
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import CakeContainer from './components/CakeContainer'
 import HooksCakeContainer from './components/HooksCakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
 import HooksIceCreamContainer from './components/HooksIceCreamContainer'
+import NewCakeContainer from './components/NewCakeContainer'
+import ItemContainer from './components/ItemContainer'
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className='App'>
-        <HooksIceCreamContainer />
-        <HooksCakeContainer />
+        <ItemContainer cake />
+        <ItemContainer iceCream />
+        {/* <NewCakeContainer /> */}
+        {/* <HooksCakeContainer /> */}
+        {/* <CakeContainer /> */}
+        {/* <HooksIceCreamContainer /> */}
+        {/* <IceCreamContainer /> */}
       </div>
     </Provider>
   );
