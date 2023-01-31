e171eaad102eb52fe2780061082c78d06bf78e6b
Lester Cuadra <lester.cuadra24@gmail.com>
2023-01-31 13:39:08 +0800

    feat: added store & necessary dependencies

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
 import CakeContainer from './components/CakeContainer'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
-export default App;
+export default App
